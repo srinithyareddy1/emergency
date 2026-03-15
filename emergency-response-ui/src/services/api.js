f54aa68c4ce11b69bed30fe5c7db4870c367e277
srinithyareddy1 <nithyareddgy569@gmail.com>
2026-03-15 16:53:30 +0530

    update api urls to render services

--- emergency-response-ui/src/services/api.js
+++ emergency-response-ui/src/services/api.js
@@ -1,14 +1,48 @@
-import axios from  "axios";
+import axios from "axios";
 
-const API=axios.create({
-    baseURL:"http://localhost:8080"
-});
-export const requestAmbulance=(data)=>{
-    return API.post("/emergency/request",data);
+/* ========================
+   MICROSERVICE URLS
+======================== */
+
+const AUTH_API = "https://auth-service-y83q.onrender.com";
+const HOSPITAL_API = "https://emergency-vpux.onrender.com";
+const EMERGENCY_API = "https://emergency-service-f5lz.onrender.com";
+
+
+/* ========================
+   AUTH SERVICE
+======================== */
+
+export const loginUser = (data) => {
+  return axios.post(`${AUTH_API}/auth/login`, data);
 };
-export const getHospitals=()=>{
-    return API.get("/hospital/available");
+
+export const registerUser = (data) => {
+  return axios.post(`${AUTH_API}/auth/register`, data);
 };
-export const getEmergencies=()=>{
-    return API.get("/emergency/all");
+
+
+/* ========================
+   HOSPITAL SERVICE
+======================== */
+
+export const getHospitals = () => {
+  return axios.get(`${HOSPITAL_API}/hospital/available`);
+};
+
+export const addHospital = (data) => {
+  return axios.post(`${HOSPITAL_API}/hospital/add`, data);
+};
+
+
+/* ========================
+   EMERGENCY SERVICE
+======================== */
+
+export const requestAmbulance = (data) => {
+  return axios.post(`${EMERGENCY_API}/emergency/request`, data);
+};
+
+export const getEmergencies = () => {
+  return axios.get(`${EMERGENCY_API}/emergency/all`);
 };
