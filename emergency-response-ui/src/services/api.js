525d55985e83d888dd8959dd9a34cc8a1218fdfe
srinithyareddy1 <nithyareddgy569@gmail.com>
2026-03-15 17:23:59 +0530

    fix signup api endpoint

--- emergency-response-ui/src/services/api.js
+++ emergency-response-ui/src/services/api.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 
-/* ========================
-   MICROSERVICE URLS
-======================== */
+/* MICROSERVICE URLS */
 
 const AUTH_API = "https://auth-service-y83q.onrender.com";
 const HOSPITAL_API = "https://emergency-vpux.onrender.com";
 const EMERGENCY_API = "https://emergency-service-f5lz.onrender.com";
 
-
-/* ========================
-   AUTH SERVICE
-======================== */
+/* AUTH SERVICE */
 
 export const loginUser = (data) => {
   return axios.post(`${AUTH_API}/auth/login`, data);
@@ -21,10 +16,7 @@ export const registerUser = (data) => {
   return axios.post(`${AUTH_API}/auth/signup`, data);
 };
 
-
-/* ========================
-   HOSPITAL SERVICE
-======================== */
+/* HOSPITAL SERVICE */
 
 export const getHospitals = () => {
   return axios.get(`${HOSPITAL_API}/hospital/available`);
@@ -34,10 +26,7 @@ export const addHospital = (data) => {
   return axios.post(`${HOSPITAL_API}/hospital/add`, data);
 };
 
-
-/* ========================
-   EMERGENCY SERVICE
-======================== */
+/* EMERGENCY SERVICE */
 
 export const requestAmbulance = (data) => {
   return axios.post(`${EMERGENCY_API}/emergency/request`, data);
